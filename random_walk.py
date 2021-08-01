37fd6ec27cd376c5ea5b0734168df4fc596ef792
Marco Valtas <mavcunha@gmail.com>
2021-08-01 00:18:43 -0400

    Update random walk to neg and pos steps

--- random_walk.py
+++ random_walk.py
@@ -7,25 +7,20 @@ iterations = 1000
 def random_walk():
     r = 0
     for _ in range(1, steps + 1):
-        # couple shorthands...
-        # r += (-1, 1)[randint(0,1)]
-        # r += randint(0, 1) or -1
-        # but a clearer way to write is:
-        if (randint(0,1) == 1):
-            r += 1
-        else:
-            r += -1
-    return abs(r)
+        r += 1 if randint(0,1) else -1
+    return r
 
-def experiements(n):
+def experiments(n):
     experiments = n
     total = 0
     for _ in range(1, experiments + 1):
         total += random_walk()
-    return total / experiments
+    return total // experiments
 
+
+avg = experiments(iterations)
 
 print(f"""Random walk in one-dimension
 Steps: {steps}
 Experiments: {iterations}
-Average: {experiements(iterations)}""")
+Average: {avg}""")
